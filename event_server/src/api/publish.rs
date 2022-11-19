cbb9a6ac6c4fbd3ad0801452c9652d75a3c684df
Doyung Yoon <steamshon@apache.org>
2022-11-19 09:59:53 +0900

    add produce on publish

--- event_server/src/api/publish.rs
+++ event_server/src/api/publish.rs
@@ -1,10 +1,14 @@
 
+use std::time::Duration;
+
 use actix_web::{
     post, HttpResponse, Responder, web, 
 };
 use jsonschema::{JSONSchema, Draft};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use rdkafka::{producer::{FutureProducer, FutureRecord, future_producer::OwnedDeliveryResult}, ClientConfig, message::{OwnedHeaders, OwnedMessage}, error::KafkaError};
+use futures::future::join_all;
 
  #[derive(Debug, Serialize, Deserialize)]
 pub struct MyRequest {
@@ -49,6 +53,35 @@ fn validate_each(compiled_schema: &JSONSchema, event: &str) -> Result<bool, serd
         }
     )
 }
+
+
+async fn produce(brokers: &str, topic_name: &str, events: Vec<Value>) -> Vec<Result<(i32, i64), (KafkaError, OwnedMessage)>> {
+    let producer: &FutureProducer = &ClientConfig::new()
+        .set("bootstrap.servers", brokers)
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Producer creation error");
+
+    let futures = events
+        .iter()
+        .map(|event| async move {
+            let record = 
+                FutureRecord::to(topic_name)
+                .key("")
+                .payload("abc");
+
+            producer
+                .send(
+                    record,
+                    Duration::from_secs(0),
+                )
+                .await
+        })
+        .collect::<Vec<_>>();
+
+    join_all(futures).await
+}
+
 
 #[post("/publish")]
 pub async fn publish(request: web::Json<MyRequest>) -> impl Responder  {

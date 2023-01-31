4de5d0428aefe36bcf63d8498d59f7904c9ff010
Doyung Yoon <steamshon@apache.org>
2023-02-01 01:14:08 +0900

    change <Image> into <img> for login

--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
         <div className="h-full px-6 text-gray-800">
           <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between xl:justify-center">
             <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
-              <Image
+              <img
                 src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
                 className="w-full"
                 alt="Sample image"

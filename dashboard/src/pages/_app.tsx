53475554f7832860c03cc3eac73d8414062156e8
Doyung Yoon <steamshon@apache.org>
2023-02-01 22:30:51 +0900

    add pageLoading on router

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -7,15 +7,33 @@ import { api } from "../utils/api";
 import { Analytics } from "@vercel/analytics/react";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
+  const [pageLoading, setPageLoading] = useState<boolean>(false);
+  useEffect(() => {
+    const handleStart = () => {
+      setPageLoading(true);
+    };
+    const handleComplete = () => {
+      setPageLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
   return (
     <SessionProvider session={session}>
       <Navbar />
-      <Component {...pageProps} />
+      {pageLoading ? <div>Loading...</div> : <Component {...pageProps} />}
+
       <Analytics />
     </SessionProvider>
   );

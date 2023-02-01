9c9eb8ef4cb9c7d706730f55bb797ab3a63628f7
Doyung Yoon <steamshon@apache.org>
2023-02-01 20:57:42 +0900

    add @vercel/analytics

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -1,11 +1,12 @@
-import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
 
 import { api } from "../utils/api";
 
-import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "../components/Navbar";
+import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -15,6 +16,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <SessionProvider session={session}>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 };

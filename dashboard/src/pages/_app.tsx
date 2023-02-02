ff0d876a2bb8757cc121f100bf3e03a35df3caf6
Doyung Yoon <steamshon@apache.org>
2023-02-02 10:09:08 +0900

    rollback _app.tsx

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -1,39 +1,20 @@
+import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
 
 import { api } from "../utils/api";
 
-import { Analytics } from "@vercel/analytics/react";
-import Navbar from "../components/Navbar";
 import "../styles/globals.css";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  const router = useRouter();
-  const [pageLoading, setPageLoading] = useState<boolean>(false);
-  useEffect(() => {
-    const handleStart = () => {
-      setPageLoading(true);
-    };
-    const handleComplete = () => {
-      setPageLoading(false);
-    };
-
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
-  }, [router]);
-
   return (
     <SessionProvider session={session}>
       <Navbar />
-      {pageLoading ? <div>Loading...</div> : <Component {...pageProps} />}
-      <Analytics />
+      <Component {...pageProps} />
     </SessionProvider>
   );
 };

c3d4f3f55e9452f51f924231d0566378612f0d96
Doyung Yoon <steamshon@apache.org>
2023-03-04 08:04:07 +0900

    cleanup tests

--- server/filter/src/index_test.rs
+++ server/filter/src/index_test.rs
@@ -18,12 +18,7 @@ impl Filterable for TestFilter {
         TargetFilter::from(&value)
     }
 }
-#[test]
-fn test_new_filter_index() {
-    let filter = TestFilter {
-        id: String::from("AD_1"),
-        filter: String::from(
-            r#"
+const FILTER_1_STR: &str = r#"
     {
         "type": "or",
         "fields": [
@@ -71,11 +66,16 @@ fn test_new_filter_index() {
                 ]
             }
         ]
-    }"#,
-        ),
-    };
-    let target_keys = build_target_keys(&filter.filter().unwrap());
-    let expected_target_keys = vec![
+    }"#;
+
+fn test_filter_1(id: &str) -> TestFilter {
+    TestFilter {
+        id: String::from(id),
+        filter: String::from(FILTER_1_STR),
+    }
+}
+fn test_filter_1_expected_target_keys() -> Vec<TargetKey> {
+    vec![
         TargetKey {
             dim_values: vec![DimValue::new("age", "10", false)],
         },
@@ -102,14 +102,10 @@ fn test_new_filter_index() {
                 DimValue::new("interests", "L1", false),
             ],
         },
-    ];
-    assert_eq!(target_keys, expected_target_keys);
-
-    let filters: Vec<TestFilter> = vec![filter];
-
-    let filter_index = FilterIndex::new(&filters);
-    let id = &filters[0].id;
-    let expected_true_index = json!({
+    ]
+}
+fn test_filter_1_expected_true_index(id: &str) -> serde_json::Value {
+    json!({
         DimValue::new("gender", "empty", false).debug():
             vec![
                 format!("{id}_{seq}", id = id, seq = "0"),
@@ -144,11 +140,40 @@ fn test_new_filter_index() {
                 format!("{id}_{seq}", id = id, seq = "0"),
                 format!("{id}_{seq}", id = id, seq = "2")
             ],
-    });
+    })
+}
+fn test_filter_1_expected_false_index(id: &str) -> serde_json::Value {
+    json!({
+        DimValue::new("interests", "L2,L3", true).debug():
+            vec![
+                format!("{id}", id = id),
+            ],
+    })
+}
+#[test]
+fn test_filter_1_index() {
+    let filter = test_filter_1("AD_1");
+    let target_keys = build_target_keys(&filter.filter().unwrap());
+    let expected_target_keys = test_filter_1_expected_target_keys();
+    assert_eq!(target_keys, expected_target_keys);
+
+    let filters: Vec<TestFilter> = vec![filter];
+
+    let filter_index = FilterIndex::new(&filters);
+    let id = &filters[0].id;
+    let expected_true_index = test_filter_1_expected_true_index(id);
     let true_index_debug = FilterIndex::debug_index(&filter_index.true_index.lock().unwrap());
     assert_eq!(true_index_debug, expected_true_index);
-
-    println!("{:?}", true_index_debug);
+    let false_index_debug = FilterIndex::debug_index(&filter_index.false_index.lock().unwrap());
+    let expected_false_index = test_filter_1_expected_false_index(id);
+    assert_eq!(false_index_debug, expected_false_index);
+}
+#[test]
+fn test_new_filter_index() {
+    let id = "AD_1";
+    let filter = test_filter_1(id);
+    let filters: Vec<TestFilter> = vec![filter];
+    let filter_index = FilterIndex::new(&filters);
     let user_info = HashMap::from([(
         String::from("age"),
         HashSet::from([String::from("10"), String::from("20")]),
@@ -157,7 +182,10 @@ fn test_new_filter_index() {
     let internal_ids = filter_index
         .search_positive_internal_ids(&user_info)
         .unwrap_or(HashSet::new());
-    let expected_internal_ids = HashSet::from([String::from("AD_1_0"), String::from("AD_1_2")]);
+    let expected_internal_ids = HashSet::from([
+        format!("{id}_{seq}", seq = "0"),
+        format!("{id}_{seq}", seq = "2"),
+    ]);
     println!("{:?}", internal_ids);
     assert_eq!(internal_ids, expected_internal_ids);
 }

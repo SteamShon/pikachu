6c16b7ebecc902820f9a91e5b9b03763677f9f98
Doyung Yoon <steamshon@apache.org>
2023-03-08 14:01:00 +0900

    add background task to update ad_meta periodically.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,13 @@
 pub mod ad_filter;
 pub mod db;
-
 use std::{
     collections::{HashMap, HashSet},
     env,
-    sync::{Arc, Mutex},
+    sync::Arc,
+    thread,
     time::Duration,
 };
+use tokio::sync::Mutex;
 
 use actix_cors::Cors;
 use actix_web::{
@@ -29,13 +30,11 @@ struct Request {
     user_info: Value,
 }
 pub async fn long_running_task(data: web::Data<Mutex<AdState>>, client: web::Data<PrismaClient>) {
-    let new_client = client.into_inner();
-
     loop {
-        let mut ad_state = data.lock().unwrap();
-        ad_state.load(new_client.clone()).await;
+        let mut ad_state = data.lock().await;
+        ad_state.load(client.clone().into_inner()).await;
 
-        tokio::time::sleep(Duration::from_secs(10)).await;
+        tokio::time::sleep(Duration::from_secs(60)).await;
     }
 }
 #[post("/update_ad_meta")]
@@ -44,27 +43,14 @@ async fn update_ad_meta(
     client: web::Data<PrismaClient>,
     // request: web::Json<placement_group::Data>,
 ) -> impl Responder {
-    let mut ad_state = data.lock().unwrap();
+    let mut ad_state = data.lock().await;
     ad_state.load(client.into_inner()).await;
-
-    HttpResponse::Ok().json(true)
-}
-#[post("/update_placement_groups")]
-async fn update_placement_groups(
-    data: web::Data<Mutex<AdState>>,
-    client: web::Data<PrismaClient>,
-    // request: web::Json<placement_group::Data>,
-) -> impl Responder {
-    let mut ad_state = data.lock().unwrap();
-    ad_state
-        .fetch_and_update_placement_groups(client.into_inner(), None)
-        .await;
 
     HttpResponse::Ok().json(true)
 }
 #[post("/search")]
 async fn search(data: web::Data<Mutex<AdState>>, request: web::Json<Request>) -> impl Responder {
-    let matched_ad_groups = data.lock().unwrap().search(
+    let matched_ad_groups = data.lock().await.search(
         &request.service_id,
         &request.placement_group_id,
         &request.user_info,
@@ -78,12 +64,14 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").unwrap();
-    let prisma = db::new_client_with_url(&database_url).await.unwrap();
-    let client = web::Data::new(prisma);
-    let state = AdState::default();
-    let ad_state = web::Data::new(Mutex::new(state));
 
-    //tokio::spawn(async move { long_running_task(ad_state.clone(), client.clone()).await });
+    let prisma = Arc::new(db::new_client_with_url(&database_url).await.unwrap());
+    let client = web::Data::from(prisma);
+
+    let state = Arc::new(Mutex::new(AdState::default()));
+    let ad_state = web::Data::from(state);
+
+    tokio::spawn(long_running_task(ad_state.clone(), client.clone()));
 
     HttpServer::new(move || {
         let cors = Cors::permissive();
@@ -93,7 +81,6 @@ async fn main() -> std::io::Result<()> {
             .app_data(ad_state.clone())
             .app_data(client.clone())
             .service(search)
-            .service(update_placement_groups)
             .service(update_ad_meta)
             .wrap(cors)
             .wrap(logger)

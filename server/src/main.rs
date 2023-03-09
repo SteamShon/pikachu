a5dae23de9d7ed6f7331645950516790295c9f1b
Doyung Yoon <steamshon@apache.org>
2023-03-10 08:44:49 +0900

    Add Dockerfile

--- server/src/main.rs
+++ server/src/main.rs
@@ -114,7 +114,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(cors)
             .wrap(logger)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }

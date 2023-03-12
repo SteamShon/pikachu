739edfc60faee7b0d50b739025ff3e8ab9aed03b
Doyung Yoon <steamshon@apache.org>
2023-03-12 22:41:51 +0900

    Add docker-compose and prisma migration

---
+++ dashboard/prisma/migrations/20230312085735_init/migration.sql
@@ -0,0 +1,400 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+    "role" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Service" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Service_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UsersOnServices" (
+    "serviceId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "assignedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UsersOnServices_pkey" PRIMARY KEY ("serviceId","userId")
+);
+
+-- CreateTable
+CREATE TABLE "PlacementGroup" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "serviceId" TEXT,
+    "cubeId" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PlacementGroup_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Placement" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "placementGroupId" TEXT,
+    "contentTypeId" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Placement_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ContentType" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "serviceId" TEXT,
+    "schema" TEXT,
+    "uiSchema" TEXT,
+    "defaultValues" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ContentType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AdvertisersOnPlacements" (
+    "advertiserId" TEXT NOT NULL,
+    "placementId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "AdvertisersOnPlacements_pkey" PRIMARY KEY ("advertiserId","placementId")
+);
+
+-- CreateTable
+CREATE TABLE "Campaign" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "placementId" TEXT NOT NULL,
+    "type" TEXT NOT NULL DEFAULT 'DISPLAY',
+    "startedAt" TIMESTAMP(3),
+    "endAt" TIMESTAMP(3),
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Campaign_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AdGroup" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "campaignId" TEXT NOT NULL,
+    "filter" TEXT,
+    "population" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "AdGroup_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Creative" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "adGroupId" TEXT NOT NULL,
+    "contentId" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Creative_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Content" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "contentTypeId" TEXT,
+    "values" TEXT NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT,
+
+    CONSTRAINT "Content_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Customset" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "serviceId" TEXT,
+    "creatorId" TEXT NOT NULL,
+    "customsetInfoId" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Customset_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CustomsetInfo" (
+    "id" TEXT NOT NULL,
+    "filePath" TEXT NOT NULL,
+    "config" TEXT NOT NULL,
+
+    CONSTRAINT "CustomsetInfo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CubeConfig" (
+    "id" TEXT NOT NULL,
+    "serviceId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "s3Region" TEXT NOT NULL,
+    "s3AccessKeyId" TEXT NOT NULL,
+    "s3SecretAccessKey" TEXT NOT NULL,
+    "buckets" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CubeConfig_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Cube" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "cubeConfigId" TEXT NOT NULL,
+    "sql" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Cube_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Segment" (
+    "id" TEXT NOT NULL,
+    "cubeId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "where" TEXT,
+    "population" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'CREATED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Segment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- CreateIndex
+CREATE INDEX "Service_updatedAt_idx" ON "Service"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Service_name_key" ON "Service"("name");
+
+-- CreateIndex
+CREATE INDEX "PlacementGroup_updatedAt_idx" ON "PlacementGroup"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PlacementGroup_serviceId_name_key" ON "PlacementGroup"("serviceId", "name");
+
+-- CreateIndex
+CREATE INDEX "Placement_updatedAt_idx" ON "Placement"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Placement_placementGroupId_name_key" ON "Placement"("placementGroupId", "name");
+
+-- CreateIndex
+CREATE INDEX "ContentType_updatedAt_idx" ON "ContentType"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ContentType_serviceId_name_key" ON "ContentType"("serviceId", "name");
+
+-- CreateIndex
+CREATE INDEX "Campaign_updatedAt_idx" ON "Campaign"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Campaign_placementId_name_key" ON "Campaign"("placementId", "name");
+
+-- CreateIndex
+CREATE INDEX "AdGroup_updatedAt_idx" ON "AdGroup"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AdGroup_campaignId_name_key" ON "AdGroup"("campaignId", "name");
+
+-- CreateIndex
+CREATE INDEX "Creative_updatedAt_idx" ON "Creative"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Creative_adGroupId_name_key" ON "Creative"("adGroupId", "name");
+
+-- CreateIndex
+CREATE INDEX "Content_updatedAt_idx" ON "Content"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Content_contentTypeId_name_key" ON "Content"("contentTypeId", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customset_customsetInfoId_key" ON "Customset"("customsetInfoId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customset_serviceId_name_key" ON "Customset"("serviceId", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CubeConfig_serviceId_name_key" ON "CubeConfig"("serviceId", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Cube_cubeConfigId_name_key" ON "Cube"("cubeConfigId", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Segment_cubeId_name_key" ON "Segment"("cubeId", "name");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UsersOnServices" ADD CONSTRAINT "UsersOnServices_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UsersOnServices" ADD CONSTRAINT "UsersOnServices_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlacementGroup" ADD CONSTRAINT "PlacementGroup_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlacementGroup" ADD CONSTRAINT "PlacementGroup_cubeId_fkey" FOREIGN KEY ("cubeId") REFERENCES "Cube"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Placement" ADD CONSTRAINT "Placement_placementGroupId_fkey" FOREIGN KEY ("placementGroupId") REFERENCES "PlacementGroup"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Placement" ADD CONSTRAINT "Placement_contentTypeId_fkey" FOREIGN KEY ("contentTypeId") REFERENCES "ContentType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ContentType" ADD CONSTRAINT "ContentType_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AdvertisersOnPlacements" ADD CONSTRAINT "AdvertisersOnPlacements_advertiserId_fkey" FOREIGN KEY ("advertiserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AdvertisersOnPlacements" ADD CONSTRAINT "AdvertisersOnPlacements_placementId_fkey" FOREIGN KEY ("placementId") REFERENCES "Placement"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Campaign" ADD CONSTRAINT "Campaign_placementId_fkey" FOREIGN KEY ("placementId") REFERENCES "Placement"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AdGroup" ADD CONSTRAINT "AdGroup_campaignId_fkey" FOREIGN KEY ("campaignId") REFERENCES "Campaign"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Creative" ADD CONSTRAINT "Creative_adGroupId_fkey" FOREIGN KEY ("adGroupId") REFERENCES "AdGroup"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Creative" ADD CONSTRAINT "Creative_contentId_fkey" FOREIGN KEY ("contentId") REFERENCES "Content"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Content" ADD CONSTRAINT "Content_contentTypeId_fkey" FOREIGN KEY ("contentTypeId") REFERENCES "ContentType"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Content" ADD CONSTRAINT "Content_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Customset" ADD CONSTRAINT "Customset_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Customset" ADD CONSTRAINT "Customset_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Customset" ADD CONSTRAINT "Customset_customsetInfoId_fkey" FOREIGN KEY ("customsetInfoId") REFERENCES "CustomsetInfo"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CubeConfig" ADD CONSTRAINT "CubeConfig_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Cube" ADD CONSTRAINT "Cube_cubeConfigId_fkey" FOREIGN KEY ("cubeConfigId") REFERENCES "CubeConfig"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Segment" ADD CONSTRAINT "Segment_cubeId_fkey" FOREIGN KEY ("cubeId") REFERENCES "Cube"("id") ON DELETE CASCADE ON UPDATE CASCADE;

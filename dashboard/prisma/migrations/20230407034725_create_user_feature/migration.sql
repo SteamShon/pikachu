f343d00c1e4607e6a8cd4eb2570886457966b8bc
Doyung Yoon <steamshon@apache.org>
2023-04-07 17:03:32 +0900

    Modify schema.prisma and related migrations

---
+++ dashboard/prisma/migrations/20230407034725_create_user_feature/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "CubeHistory" (
+    "id" TEXT NOT NULL,
+    "cubeId" TEXT NOT NULL,
+    "version" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CubeHistory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserFeature" (
+    "cubeHistoryId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "feature" JSONB,
+
+    CONSTRAINT "UserFeature_pkey" PRIMARY KEY ("cubeHistoryId","userId")
+) PARTITION BY LIST ("cubeHistoryId");
+
+-- CreateIndex
+CREATE INDEX "CubeHistory_updatedAt_idx" ON "CubeHistory"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CubeHistory_cubeId_version_key" ON "CubeHistory"("cubeId", "version" DESC);
+
+-- AddForeignKey
+ALTER TABLE "CubeHistory" ADD CONSTRAINT "CubeHistory_cubeId_fkey" FOREIGN KEY ("cubeId") REFERENCES "Cube"("id") ON DELETE CASCADE ON UPDATE CASCADE;

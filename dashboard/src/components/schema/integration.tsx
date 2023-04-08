617d315edc2d0acf20eb7c8867b2ade29796bb78
Doyung Yoon <steamshon@apache.org>
2023-04-08 22:50:02 +0900

    add zod schema

---
+++ dashboard/src/components/schema/integration.tsx
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { integrationInfoSchema } from "./integrationInfo";
+
+export const integrationSchema = z.object({
+  id: z.string().optional(),
+  name: z.string().min(1),
+  description: z.string().optional().nullable().default(null),
+  type: z.enum(["DB", "HTTP"]),
+  status: z.string().min(1),
+  placementId: z.string().min(1),
+  integrationInfo: integrationInfoSchema.optional(),
+});
+
+export type IntegrationSchemaType = z.infer<typeof integrationSchema>;
---
+++ dashboard/src/components/schema/integrationInfo.tsx
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const integrationInfoSchema = z
+  .object({
+    id: z.string().optional(),
+    integrationId: z.string().min(1),
+    details: z.optional(z.record(z.unknown())),
+  })
+  .transform((o) => {
+    return {
+      ...o,
+      integrationId: o.integrationId === "" ? undefined : o.integrationId,
+    };
+  });
+
+export type IntegrationInfoSchemaType = z.infer<typeof integrationInfoSchema>;

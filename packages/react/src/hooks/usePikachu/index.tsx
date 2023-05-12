a984aca40a57889ef56d0d6058e9699199d8d280
shon <shon@shonui-MacBookPro.local>
2023-05-12 15:25:25 +0900

    change event schema on sdk

--- packages/react/src/hooks/usePikachu/index.tsx
+++ packages/react/src/hooks/usePikachu/index.tsx
@@ -26,6 +26,7 @@ export const usePikachu = ({
   serviceId,
   placementId,
   eventEndpoint,
+  eventTopic,
   options,
   debug
 }:{
@@ -33,6 +34,7 @@ export const usePikachu = ({
   serviceId: string,
   placementId: string,
   eventEndpoint?: string,
+  eventTopic?: string,
   options?: { 
     threshold: number; 
     triggerOnce: boolean; 
@@ -61,17 +63,18 @@ export const usePikachu = ({
 
         // send impression event
         const payload = { 
-          userId: userInfo.userId, 
-          event: 'impression', 
-          creativeId: nodeId,
-          timestamp: Date.now(), 
+          who: userInfo.userId, 
+          what: 'impression', 
+          which: nodeId,
+          when: Date.now(), 
         };
         if (debug) {
           console.log(payload);
         }
         if (eventEndpoint) {
+          const topic = eventTopic || "events";
           axios
-            .post(`${eventEndpoint}/${serviceId}`, payload)
+            .post(`${eventEndpoint}/${topic}/${serviceId}`, payload)
             .then((res) => console.log(res))
             .catch((e) => console.log(e))
         }
